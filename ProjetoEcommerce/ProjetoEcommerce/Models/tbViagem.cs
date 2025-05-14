feedad596c3536faa490ce212d8a31b62d4d0e56
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-14 15:13:06 -0300

    realizado o desenvolvimento da model tbViagem

--- ProjetoEcommerce/ProjetoEcommerce/Models/tbViagem.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Models/tbViagem.cs
@@ -2,5 +2,15 @@
 {
     public class tbViagem
     {
+        public int? IdViagem { get; set; }
+        public DateTime? DataRetorno { get; set; } 
+        public string? Descricao { get; set; }
+        public string? Origem { get; set; }
+        public string? Destino { get; set; }
+        public string? TipoTransporte { get; set; }
+
+        public DateTime ?DataPartida { get; set; }
+
+
     }
 }
