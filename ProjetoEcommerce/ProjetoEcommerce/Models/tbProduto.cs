df3431573d451932f1da63cbe5c994e863cd7a3b
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-14 15:08:00 -0300

    realizado o desenvolvimendo da model tbProduto

--- ProjetoEcommerce/ProjetoEcommerce/Models/tbProduto.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Models/tbProduto.cs
@@ -2,5 +2,11 @@
 {
     public class tbProduto
     {
+        public int IdProduto { get; set; }
+        public string NomeProduto {  get; set; }
+        public decimal? Valor { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade {  get; set; }
+        public List<tbProduto> ListaProduto { get; set; }
     }
 }
