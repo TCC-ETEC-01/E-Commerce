b52a9bee6f190cd17bc24c91f1f1d89a635f2370
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-19 13:39:48 -0300

    adicionando model de viagem

--- ProjetoEcommerce/ProjetoEcommerce/Models/tbViagem.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Models/tbViagem.cs
@@ -6,7 +6,7 @@
         public DateTime? DataRetorno { get; set; } 
         public string? Descricao { get; set; }
         public string? Origem { get; set; }
-        public string? Destino { get; set; }
+        public string? Destino { get; set; }2
         public string? TipoTransporte { get; set; }
 
         public DateTime ?DataPartida { get; set; }
