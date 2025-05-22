e3acfa0b36ed3913ab1eb66b7907adff04d0bb13
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-22 17:10:48 -0300

    desenvolvido as models Pacote e Log

--- ProjetoEcommerce/ProjetoEcommerce/Models/tbPacote.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Models/tbPacote.cs
@@ -2,5 +2,11 @@
 {
     public class tbPacote
     {
+        public int IdPacote { get; set; }
+        public int IdProduto { get; set; }
+        public int IdPassagem { get; set; }
+        public string NomePacote { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor {  get; set; }
     }
 }
