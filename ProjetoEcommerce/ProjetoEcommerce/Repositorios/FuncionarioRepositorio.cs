010186f282cb3efd1c8d25c182994ec2d69afddb
Gabriel <gabrielcorolleite@gmail.com>
2025-05-22 15:32:14 -0300

    Devenvolvendo o metodo EditarFuncionario

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/FuncionarioRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/FuncionarioRepositorio.cs
@@ -26,5 +26,34 @@ namespace ProjetoEcommerce.Repositorios
                 cmd.ExecuteNonQuery();
             }
         }
+
+        public bool EditarFuncionario(tbFuncionario funcionario)
+        {
+            try
+            {
+                using (var conexao = new MySqlConnection(_conexaoMySQL))
+                {
+                    conexao.Open();
+                    MySqlCommand cmd = new MySqlCommand("update tbFuncionario set Nome=@nome,Sexo=@sexo,Email=@email,Telefone=@telefone,Cargo=@cargo,Cpf=@cpf,Senha=@senha" + "where IdFuncionario=@IdFuncionario", conexao);
+                    cmd.Parameters.Add("@IdFuncionario", MySqlDbType.Int32).Value = funcionario.IdFuncionario;
+                    cmd.Parameters.Add("@Nome", MySqlDbType.VarChar).Value = funcionario.Nome;
+                    cmd.Parameters.Add("@Sexo", MySqlDbType.VarChar).Value = funcionario.Sexo;
+                    cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = funcionario.Email;
+                    cmd.Parameters.Add("@Telefone", MySqlDbType.VarChar).Value = funcionario.Telefone;
+                    cmd.Parameters.Add("@Cargo", MySqlDbType.VarChar).Value = funcionario.Cargo;
+                    cmd.Parameters.Add("@Cpf", MySqlDbType.VarChar).Value = funcionario.Cpf;
+                    cmd.Parameters.Add("@Senha", MySqlDbType.VarChar).Value = funcionario.Senha;
+                    int linhaAfestadas = cmd.ExecuteNonQuery();
+                    return linhaAfestadas > 0;
+
+                }
+            }
+
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Erro ao atualizar o funcionario: {ex.Message}");
+                return false;
+            }
+        }
     }
 }
