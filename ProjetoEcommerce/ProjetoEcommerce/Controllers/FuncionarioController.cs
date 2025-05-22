9b637ad4ea246af110aaa3fead67452b9d78d0ba
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-22 13:50:21 -0300

    adicionando a view EditarFuncionario

--- ProjetoEcommerce/ProjetoEcommerce/Controllers/FuncionarioController.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Controllers/FuncionarioController.cs
@@ -24,5 +24,10 @@ namespace ProjetoEcommerce.Controllers
             return View();
         }
 
+        public IActionResult EditarFuncionario()
+        {
+            return View();
+        }
+
     }
 }
