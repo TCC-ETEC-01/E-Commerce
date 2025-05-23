ab6bf3a09489adef5256bd805abcc78b212533ae
Dangar-dev <danielsobraldev@gmail.com>
2025-05-23 14:56:56 -0300

    Estilizando a view Funcionario Login pt2

--- ProjetoEcommerce/ProjetoEcommerce/Models/tbPacote.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Models/tbPacote.cs
@@ -3,14 +3,12 @@
     public class tbPacote
     {
         public int IdPacote { get; set; }
-//<<<<<<< HEAD
 
-//=======
         public int IdProduto { get; set; }
         public int IdPassagem { get; set; }
         public string NomePacote { get; set; }
         public string Descricao { get; set; }
         public decimal Valor {  get; set; }
-//>>>>>>> CauaDesenvolvendoModels
+
     }
 }
