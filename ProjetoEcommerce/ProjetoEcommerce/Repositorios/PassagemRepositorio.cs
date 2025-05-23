bbc271d20a9f73f705caf13f8fb91614eb5a29dd
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-23 17:46:16 -0300

    retirando variaveis extra do método de cadastrar

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/PassagemRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/PassagemRepositorio.cs
@@ -10,13 +10,10 @@ namespace ProjetoEcommerce.Repositorios
 
         public bool CadastrarPassagem(tbPassagem passagem)
         {
-            tbViagem viagem = new tbViagem();
 
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
                 conexao.Open();
-
-                bool idExistente = false;
                 MySqlCommand cmdBuscarId = new MySqlCommand("select * from tbViagem where IdViagem=@idViagem", conexao);
                 cmdBuscarId.Parameters.AddWithValue("@idViagem", passagem.IdViagem);
 
@@ -27,18 +24,15 @@ namespace ProjetoEcommerce.Repositorios
                         Console.WriteLine("Id da viagem não existente");
                         return false;
                     }
+                
                     else if (drViagem.HasRows)
                     {
-                        idExistente = true;
-
-                        bool duplicado = false;
                         MySqlCommand cmd = new MySqlCommand("select * from tbPassagem where Assento=@assento", conexao);
                         cmd.Parameters.AddWithValue("@assento", passagem.Assento);
                         using (var drAssento = cmd.ExecuteReader())
                         {
                             if (drAssento.HasRows)
                             {
-                                duplicado = true;
                                 Console.WriteLine("Assento já existente");
                                 return false;
                             }
@@ -57,6 +51,7 @@ namespace ProjetoEcommerce.Repositorios
                 }
                 conexao.Close();
             }
+
         }
     }
 }
