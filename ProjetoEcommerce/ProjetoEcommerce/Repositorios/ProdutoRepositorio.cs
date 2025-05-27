1d16c2bede2af771df6a2f2c6b744ff3cb91c089
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-27 16:20:12 -0300

    adicionado a inserção de dados no método de cadastrarProduto

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/ProdutoRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/ProdutoRepositorio.cs
@@ -12,7 +12,11 @@ namespace ProjetoEcommerce.Repositorios
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
                 conexao.Open();
-                MySqlCommand cmd = new MySqlCommand("insert into tbProduto NomeProduto")
+                MySqlCommand cmd = new MySqlCommand("insert into tbProduto (NomeProduto, Valor, Descricao, Quantidade) values(@nomeProduto,@valor, @descricao, @quantidade)", conexao);
+                cmd.Parameters.AddWithValue("@nomeProduto", produto.NomeProduto);
+                cmd.Parameters.AddWithValue("@valor", produto.Valor);
+                cmd.Parameters.AddWithValue("@descricao", produto.Descricao);
+                cmd.Parameters.AddWithValue("@quantidade", produto.Quantidade);
             }
         }
     }
