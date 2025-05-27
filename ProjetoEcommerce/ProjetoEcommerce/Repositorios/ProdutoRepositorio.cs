a7d5cf6561151198cb260013afacf9a81be0c0fb
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-27 16:16:51 -0300

    desenvolvendo o método de cadastar produto

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/ProdutoRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/ProdutoRepositorio.cs
@@ -1,6 +1,19 @@
-﻿namespace ProjetoEcommerce.Repositorios
+﻿using MySql.Data.MySqlClient;
+using ProjetoEcommerce.Models;
+
+namespace ProjetoEcommerce.Repositorios
 {
-    public class ProdutoRepositorio
+    public class ProdutoRepositorio(IConfiguration configuration)
     {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
+
+        public void CadastrarProduto(tbProduto produto)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("insert into tbProduto NomeProduto")
+            }
+        }
     }
 }
