e295e0bb57472cc321f977e3e744dae74574c2cc
Dangar-dev <danielsobraldev@gmail.com>
2025-05-28 15:15:30 -0300

    Arrumando o Funcionario Login

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/ClienteRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/ClienteRepositorio.cs
@@ -1,56 +1,22 @@
-﻿
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
 using System.Data;
 using ProjetoEcommerce.Models;
 using System.Net.Mail;
 
 namespace ProjetoEcommerce.Repositorios
 {
-  public class ClienteRepositorio (IConfiguration configuration)
-  {
-      private readonly string _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
+    public class ClienteRepositorio(IConfiguration configuration)
+    {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
 
-      public tbCliente ObterCliente(string email)
-      {
-          using (var conexao = new MySqlConnection(_conexaoMySQL))
-          {
-              conexao.Open();
-              MySqlCommand cmd = new("select * from tbCliente where Email = @email", conexao);
-              cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+        public tbCliente ObterCliente(string email)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new("select * from tbCliente where Email = @email", conexao);
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
 
-<<<<<<< HEAD
-              using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
-              {
-                  tbCliente cliente = null;
-                  if (dr.Read())
-                  {
-                      cliente = new tbCliente
-                      {
-                          Nome = dr["Nome"].ToString(),
-                          Sexo = dr["Sexo"].ToString(),
-                          Email = dr["Email"].ToString(),
-                          Telefone = dr["Email"].ToString(),
-                          Cpf = dr["Cpf"].ToString(),
-                          IdCliente = Convert.ToInt32(dr["IdCliente"])
-                      };
-                  }
-                  return cliente;
-              }
-          }
-      }
-      public void CadastrarCliente (tbCliente cliente)
-      {
-          using (var conexao = new MySqlConnection(_conexaoMySQL))
-          {
-              conexao.Open();
-              MySqlCommand cmd = new MySqlCommand("insert into tbCliente (Nome,Sexo,Email,Telefone,Cpf)Values(@nome,@sexo,@email,@telefone,@cpf)", conexao);
-              cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = cliente.Nome;
-              cmd.Parameters.Add("@sexo", MySqlDbType.VarChar).Value = cliente.Sexo;
-              cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = cliente.Email;
-              cmd.Parameters.Add("@telefone", MySqlDbType.VarChar).Value = cliente.Telefone;
-              cmd.Parameters.Add("@cpf", MySqlDbType.VarChar).Value = cliente.Cpf;
-              cmd.ExecuteNonQuery();
-=======
                 using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
                 {
                     tbCliente cliente = null;
@@ -70,7 +36,7 @@ namespace ProjetoEcommerce.Repositorios
                 }
             }
         }
-        public bool CadastrarCliente (tbCliente cliente)
+        public bool CadastrarCliente(tbCliente cliente)
         {
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
@@ -79,7 +45,7 @@ namespace ProjetoEcommerce.Repositorios
                 MySqlCommand verify = new MySqlCommand("select tbCliente where Email=@email", conexao);
                 verify.Parameters.AddWithValue("@email", cliente);
                 using (var conf = verify.ExecuteReader())
-                { 
+                {
                     if (conf.HasRows)
                     {
                         Console.WriteLine("Email ja cadastrado!");
@@ -115,73 +81,50 @@ namespace ProjetoEcommerce.Repositorios
                 cmd.Parameters.Add("@cpf", MySqlDbType.VarChar).Value = cliente.Cpf;
                 cmd.ExecuteNonQuery();
                 return true;
->>>>>>> GabrielDev
 
-          }  
-      }
+            }
+        }
 
-      public bool EditarCliente(tbCliente cliente)
-      {
-          try
-          {
+        public bool EditarCliente(tbCliente cliente)
+        {
+            try
+            {
 
-              using (var conexao = new MySqlConnection(_conexaoMySQL))
-              {
-                  conexao.Open();
-                  MySqlCommand cmd = new MySqlCommand("update tbCliente set Nome=@nome,Sexo=@sexo,Email=@email,Telefone=@telefone,Cpf=@cpf" +
-                      "where IdCliente=@IdCliente", conexao);
-                  cmd.Parameters.Add("@IdCliente", MySqlDbType.Int32).Value = cliente.IdCliente;
-                  cmd.Parameters.Add("@Nome", MySqlDbType.VarChar).Value = cliente.Nome;
-                  cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = cliente.Email;
-                  cmd.Parameters.Add("@Telefone", MySqlDbType.VarChar).Value = cliente.Telefone;
-                  cmd.Parameters.Add("@Cpf", MySqlDbType.VarChar).Value = cliente.Cpf;
-                  int linhasAfestadas = cmd.ExecuteNonQuery();
-                  return linhasAfestadas > 0;
-              }
-          }
-          catch (Exception ex)
-          {
-              Console.WriteLine($"Erro ao atualizar o funcionario: {ex.Message}");
-              return false;
+                using (var conexao = new MySqlConnection(_conexaoMySQL))
+                {
+                    conexao.Open();
+                    MySqlCommand cmd = new MySqlCommand("update tbCliente set Nome=@nome,Sexo=@sexo,Email=@email,Telefone=@telefone,Cpf=@cpf" +
+                        "where IdCliente=@IdCliente", conexao);
+                    cmd.Parameters.Add("@IdCliente", MySqlDbType.Int32).Value = cliente.IdCliente;
+                    cmd.Parameters.Add("@Nome", MySqlDbType.VarChar).Value = cliente.Nome;
+                    cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = cliente.Email;
+                    cmd.Parameters.Add("@Telefone", MySqlDbType.VarChar).Value = cliente.Telefone;
+                    cmd.Parameters.Add("@Cpf", MySqlDbType.VarChar).Value = cliente.Cpf;
+                    int linhasAfestadas = cmd.ExecuteNonQuery();
+                    return linhasAfestadas > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao atualizar o funcionario: {ex.Message}");
+                return false;
 
-          }
-      }
+            }
+        }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-      public IEnumerable<tbCliente> TodosClientes()
-      {
-          List<tbCliente> ListaCliente = new List<tbCliente>();
-          using (var conexao = new MySqlConnection(_conexaoMySQL))
-          {
-              conexao.Open();
-              MySqlCommand cmd = new MySqlCommand("select * from tbCliente", conexao);
-              MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-              DataTable dt = new DataTable();
-=======
-      //public IEnumerable<tbCliente> TodosClientes()
-      //{
-      //    List<tbCliente> ListaCliente = new List<tbCliente>();
-      //    using (var conexao = new MySqlConnection(_conexaoMySQL))
-      //    {
-      //        conexao.Open();
-      //        MySqlCommand cmd = new MySqlCommand("select * from tbCliente", conexao);
-      //        MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-      //        DataTable dt = new DataTable();
->>>>>>> Daniel-dev
+        public IEnumerable<tbCliente> TodosClientes()
+        {
+            List<tbCliente> ListaCliente = new List<tbCliente>();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from tbCliente", conexao);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
 
-      //        da.Fill(dt);
-      //        conexao.Close();
+                da.Fill(dt);
+                conexao.Close();
 
-      //        foreach (DataRow dr in dt.Rows)
-      //        {
-      //            ListaCliente
-      //        }
-          //}
-
-      //}
-  }
-=======
                 foreach (DataRow dr in dt.Rows)
                 {
                     ListaCliente.Add(new tbCliente
@@ -196,7 +139,7 @@ namespace ProjetoEcommerce.Repositorios
                 return ListaCliente;
             }
         }
-        public void ExcluirCliente( int id)
+        public void ExcluirCliente(int id)
         {
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
@@ -208,5 +151,4 @@ namespace ProjetoEcommerce.Repositorios
             }
         }
     }
->>>>>>> GabrielDev
 }
