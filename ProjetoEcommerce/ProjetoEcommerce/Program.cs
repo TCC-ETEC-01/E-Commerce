71c8b114c918a9f6e9b4650f2ec405a862e94286
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-31 16:31:46 -0300

    alterando o program para iniciar na tela de login

--- ProjetoEcommerce/ProjetoEcommerce/Program.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Funcionario}/{action=Login}/{id?}");
 
 app.Run();
