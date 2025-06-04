8a4d07a1967d912941cad807069da7c86b8a2a02
Gabriel <gabrielcorolleite@gmail.com>
2025-06-04 20:41:54 -0300

    Desenvolvendo a ViagemController

--- ProjetoEcommerce/ProjetoEcommerce/Controllers/ViagemController.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Controllers/ViagemController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoEcommerce.Repositorios;
 
 namespace ProjetoEcommerce.Controllers
 {
     public class ViagemController : Controller
     {
+        private readonly ViagemRepositorio _viagemRepositorio;
+
+        public ViagemController(ViagemRepositorio viagemRepositorio)
+        {
+            _viagemRepositorio = viagemRepositorio;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_viagemRepositorio.TodasViagens());
         }
     }
 }
