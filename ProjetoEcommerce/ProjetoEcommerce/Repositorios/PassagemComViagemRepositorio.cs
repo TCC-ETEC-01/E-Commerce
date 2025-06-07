610fe8d3516227df0dea5a8f728bf47ef16f44e6
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-07 14:38:01 -0300

    desenvolvido a query do inner join do repositorio PassagemComViagem

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/PassagemComViagemRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/PassagemComViagemRepositorio.cs
@@ -1,9 +1,25 @@
-﻿namespace ProjetoEcommerce.Repositorios
+﻿using MySql.Data.MySqlClient;
+using ProjetoEcommerce.Models;
+
+namespace ProjetoEcommerce.Repositorios
 {
     public class PassagemComViagemRepositorio(IConfiguration configuration)
     {
         private readonly string _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
 
-        public IEnumerable<tbPassagemComViagem>
+        public IEnumerable<tbPassagemComViagem> PassagemComViagem()
+        {
+            var lista = new List<tbPassagemComViagem>();
+
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                string query = @"select  p.IdPassagem,v.Origem, v.Destino, p.Assento,v.Descricao,  
+                    v.DataPartida, v.DataRetorno as Data_Retorno,  v.TipoTransporte
+                    from tbPassagem p 
+                    inner join tbViagem v on p.IdViagem = v.IdViagem;";
+
+            }
+        }
     }
 }
