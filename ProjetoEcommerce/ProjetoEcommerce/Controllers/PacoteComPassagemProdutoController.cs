2aa6af8da0cebc63b8fd37013fba8e05c86808de
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-07 15:10:02 -0300

    excluindo controller e adicionando nova, desenvolvido o construtor

---
+++ ProjetoEcommerce/ProjetoEcommerce/Controllers/PacoteComPassagemProdutoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoEcommerce.Repositorios;
+namespace ProjetoEcommerce.Controllers
+{
+    public class PacoteComPassagemProdutoController : Controller
+    {
+        private readonly PacoteComPassagemProdutoRepositorio _pacoteComPassagemProdutoRepositorio;
+
+        public PacoteComPassagemProdutoController(PacoteComPassagemProdutoRepositorio pacoteComPassagemProdutoRepositorio)
+        {
+            _pacoteComPassagemProdutoRepositorio = pacoteComPassagemProdutoRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
