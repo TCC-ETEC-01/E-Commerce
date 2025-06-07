567b0da53de524f6f09fbf392b0314abd14f4ff4
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-07 14:45:40 -0300

    desenvolvido a listagem de PassagemComViagem

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/PassagemComViagemRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/PassagemComViagemRepositorio.cs
@@ -15,9 +15,29 @@ namespace ProjetoEcommerce.Repositorios
             {
                 conexao.Open();
                 string query = @"select  p.IdPassagem,v.Origem, v.Destino, p.Assento,v.Descricao,  
-                    v.DataPartida, v.DataRetorno as Data_Retorno,  v.TipoTransporte
+                    v.DataPartida as Partida, v.DataRetorno as Retorno,  v.TipoTransporte as Transporte
                     from tbPassagem p 
                     inner join tbViagem v on p.IdViagem = v.IdViagem;";
+                using(MySqlCommand join = new MySqlCommand(query, conexao))
+                {
+                    using (MySqlDataReader drPassagemComViagem =  join.ExecuteReader())
+                    {
+                        while(drPassagemComViagem.Read())
+                        {
+                            lista.Add(new tbPassagemComViagem
+                            {
+                                IdPassagem = drPassagemComViagem.GetInt32("IdPassagem"),
+                                Origem = drPassagemComViagem.GetString("Origem"),
+                                Destino = drPassagemComViagem.GetString("Assento"),
+                                Descricao = drPassagemComViagem.GetString("Descricao"),
+                                DataPartida = drPassagemComViagem.GetDateTime("Partida"),
+                                DataRetorno = drPassagemComViagem.GetDateTime("Retorno"),
+                                TipoTransporte = drPassagemComViagem.GetString("Transporte")
+                            });
+                        }
+                    }
+                    return lista;
+                }
 
             }
         }
