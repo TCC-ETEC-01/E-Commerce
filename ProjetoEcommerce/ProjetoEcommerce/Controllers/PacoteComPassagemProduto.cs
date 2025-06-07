0719e91d2411c7cebb1e0c1f7c9e2490482d8394
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-06 23:26:04 -0300

    adicionando view do join PacoteComPassagemProduto

---
+++ ProjetoEcommerce/ProjetoEcommerce/Controllers/PacoteComPassagemProduto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoEcommerce.Controllers
+{
+    public class PacoteComPassagemProduto : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
