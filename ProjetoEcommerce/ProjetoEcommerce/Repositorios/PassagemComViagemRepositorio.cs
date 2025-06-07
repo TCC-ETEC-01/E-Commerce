83346dee5cd4b976017184d3129ac474042c11f7
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-07 15:05:59 -0300

    adicionando apelido para IdPassagem

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/PassagemComViagemRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/PassagemComViagemRepositorio.cs
@@ -14,19 +14,19 @@ namespace ProjetoEcommerce.Repositorios
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
                 conexao.Open();
-                string query = @"select  p.IdPassagem,v.Origem, v.Destino, p.Assento,p.Translado, v.Descricao,  
+                string query = @"select  p.IdPassagem as Codigo,v.Origem, v.Destino, p.Assento,p.Translado, v.Descricao,  
                     v.DataPartida as Partida, v.DataRetorno as Retorno,  v.TipoTransporte as Transporte
                     from tbPassagem p 
                     inner join tbViagem v on p.IdViagem = v.IdViagem;";
-                using(MySqlCommand join = new MySqlCommand(query, conexao))
+                using (MySqlCommand join = new MySqlCommand(query, conexao))
                 {
-                    using (MySqlDataReader drPassagemComViagem =  join.ExecuteReader())
+                    using (MySqlDataReader drPassagemComViagem = join.ExecuteReader())
                     {
-                        while(drPassagemComViagem.Read())
+                        while (drPassagemComViagem.Read())
                         {
                             lista.Add(new tbPassagemComViagem
                             {
-                                IdPassagem = drPassagemComViagem.GetInt32("IdPassagem"),
+                                IdPassagem = drPassagemComViagem.GetInt32("Codigo"),
                                 Origem = drPassagemComViagem.GetString("Origem"),
                                 Destino = drPassagemComViagem.GetString("Assento"),
                                 Descricao = drPassagemComViagem.GetString("Descricao"),
@@ -38,7 +38,6 @@ namespace ProjetoEcommerce.Repositorios
                     }
                     return lista;
                 }
-
             }
         }
     }
