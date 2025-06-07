f4785224c8a09045064ddd1b8536683a6e06919b
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-07 14:09:26 -0300

    adicionando model para ClienteComPassagem

--- ProjetoEcommerce/ProjetoEcommerce/Models/tbClienteComPassagem.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Models/tbClienteComPassagem.cs
@@ -2,5 +2,16 @@
 {
     public class tbClienteComPassagem
     {
+        public int IdPassagem { get; set; }
+        public string Assento { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCompra { get; set; }
+        public int IdViagem { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+        public string Situacao { get; set; }
+        public string Translado { get; set; }
     }
 }
--- ProjetoEcommerce/ProjetoEcommerce/Models/tbPassagem.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Models/tbPassagem.cs
@@ -5,10 +5,10 @@
         public int IdPassagem { get; set; }
         public string Assento { get; set; }
         public decimal Valor { get; set; }
-        public DateTime DataCompra {  get; set; }
+        public DateTime DataCompra { get; set; }
         public int IdViagem { get; set; }
         public int IdCliente { get; set; }
         public string Situacao { get; set; }
-        public string Translado { get; set;
+        public string Translado{get; set;}
     }
 }
