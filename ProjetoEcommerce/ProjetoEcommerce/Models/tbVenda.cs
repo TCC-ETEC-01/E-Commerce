9319e0a456cc8c3bd4497e7f1e6ca83922b55e6c
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-08 11:51:49 -0300

    adicionando model tbVenda

---
+++ ProjetoEcommerce/ProjetoEcommerce/Models/tbVenda.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetoEcommerce.Models
+{
+    public class tbVenda
+    {
+        public tbPassagem IdPassagem { get; set; }
+        public tbCliente IdCliente { get; set; }
+        public tbProduto IdProduto { get; set; }
+        public int IdVenda { get; set; }
+        public decimal Valor { get; set; }
+        public tbFuncionario IdFuncionario { get; set; }
+    }
+}
