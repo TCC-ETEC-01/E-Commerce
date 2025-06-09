79dc9871b793549ddcea9916a372a5febb2c1b8b
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-09 15:25:38 -0300

    adicionando a model tbTransporte

---
+++ ProjetoEcommerce/ProjetoEcommerce/Models/tbTransporte.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace ProjetoEcommerce.Models
+{
+    public class tbTransporte
+    {
+        public int IdTransporte { get; set; }
+        public string CodigoTransporte { get; set; }
+        public string Companhia { get; set; }
+        public string TipoTransporte { get; set; }
+    }
+}
