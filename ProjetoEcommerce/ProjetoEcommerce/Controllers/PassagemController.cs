d223e4095f507de79253a48e905c341e0f24fa21
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-11 11:00:22 -0300

    alterado os métodos da controller Passagem para async

--- ProjetoEcommerce/ProjetoEcommerce/Controllers/PassagemController.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Controllers/PassagemController.cs
@@ -1,37 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProjetoEcommerce.Models;
 using ProjetoEcommerce.Repositorios;
+using System.Threading.Tasks;
 
 namespace ProjetoEcommerce.Controllers
 {
     public class PassagemController : Controller
     {
         private readonly PassagemRepositorio _passagemRepositorio;
-        
+
         public PassagemController(PassagemRepositorio passagemRepositorio)
         {
             _passagemRepositorio = passagemRepositorio;
         }
-     
+
         public IActionResult CadastrarPassagem()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult CadastrarPassagem(tbPassagem passagem)
+        public async Task<IActionResult> CadastrarPassagem(tbPassagem passagem)
         {
-            if(_passagemRepositorio.CadastrarPassagem(passagem))
+            if (await _passagemRepositorio.CadastrarPassagemAsync(passagem))
             {
                 TempData["MensagemSucesso"] = "Passagem cadastrada com Sucesso";
-                return RedirectToAction(nameof(Index));  
+                return RedirectToAction(nameof(Index));
             }
             TempData["MensagemErro"] = "Erro ao cadastrar passagem";
             return View(passagem);
         }
-        public IActionResult EditarPassagem(int id)
+
+        public async Task<IActionResult> EditarPassagem(int id)
         {
-            var passagem = _passagemRepositorio.ObterPassagem(id);
+            var passagem = await _passagemRepositorio.ObterPassagemAsync(id);
 
             if (passagem == null)
             {
@@ -41,16 +43,17 @@ namespace ProjetoEcommerce.Controllers
         }
 
         [HttpPost]
-        public IActionResult EditarPassagem(int id, [Bind("IdPassagem, Valor, Assento, IdViagem, Situacao")] tbPassagem passagem)
+        public async Task<IActionResult> EditarPassagem(int id, [Bind("IdPassagem, Valor, Assento, IdViagem, Situacao")] tbPassagem passagem)
         {
             ModelState.Clear();
-            if(id != passagem.IdPassagem)
+            if (id != passagem.IdPassagem)
             {
                 return BadRequest();
             }
-                if(ModelState.IsValid)
+
+            if (ModelState.IsValid)
             {
-                if(_passagemRepositorio.AtualizarPassagem(passagem))
+                if (await _passagemRepositorio.AtualizarPassagemAsync(passagem))
                 {
                     TempData["MensagemSucesso"] = "Passagem atualizada com Sucesso";
                     return RedirectToAction(nameof(Index));
@@ -58,13 +61,14 @@ namespace ProjetoEcommerce.Controllers
                 TempData["MensagemErro"] = "Erro ao atualizar passagem";
                 return View(passagem);
             }
-                
+
             return View();
         }
-        public IActionResult ExcluirPassagem(int id)
+
+        public async Task<IActionResult> ExcluirPassagem(int id)
         {
-            TempData["MensagemSucesso"] = "Passagem excluida com Sucesso";
-            _passagemRepositorio.ExcluirPassagem(id);
+            await _passagemRepositorio.ExcluirPassagemAsync(id);
+            TempData["MensagemSucesso"] = "Passagem excluída com Sucesso";
             return RedirectToAction(nameof(Index));
         }
 
