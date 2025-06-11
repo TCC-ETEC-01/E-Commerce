5b1847bb295f415ba02f588ad76dd03789d68dcc
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-11 11:10:51 -0300

    alterado os métodos do repositorio Transporte para async

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/TransporteRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/TransporteRepositorio.cs
@@ -1,101 +1,105 @@
 ﻿using MySql.Data.MySqlClient;
 using ProjetoEcommerce.Models;
+using System.Data;
 
 namespace ProjetoEcommerce.Repositorios
 {
-    public class TransporteRepositorio(IConfiguration configuration)
+    public class TransporteRepositorio
     {
-            private readonly string _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
+        private readonly string _conexaoMySQL;
 
-            public bool CadastrarTransporte(tbTransporte transporte)
-            {
-                using (var conexao = new MySqlConnection(_conexaoMySQL))
-                {
-                    conexao.Open();
-                    var cmd = new MySqlCommand("insert into tbTransporte (CodigoTransporte, Companhia, TipoTransporte) VALUES (@codigo, @companhia, @tipo)", conexao);
-                    cmd.Parameters.AddWithValue("@codigo", transporte.CodigoTransporte);
-                    cmd.Parameters.AddWithValue("@companhia", transporte.Companhia);
-                    cmd.Parameters.AddWithValue("@tipo", transporte.TipoTransporte);
-                    int linhasAfetadas = cmd.ExecuteNonQuery();
-                    return linhasAfetadas > 0;
-                }
-            }
-
-            public bool AtualizarTransporte(tbTransporte transporte)
-            {
-                using (var conexao = new MySqlConnection(_conexaoMySQL))
-                {
-                    conexao.Open();
-                    var cmd = new MySqlCommand("update tbTransporte setT CodigoTransporte=@codigo, Companhia=@companhia, TipoTransporte=@tipo WHERE IdTransporte=@id", conexao);
-                    cmd.Parameters.AddWithValue("@codigo", transporte.CodigoTransporte);
-                    cmd.Parameters.AddWithValue("@companhia", transporte.Companhia);
-                    cmd.Parameters.AddWithValue("@tipo", transporte.TipoTransporte);
-                    cmd.Parameters.AddWithValue("@id", transporte.IdTransporte);
-                    int linhasAfetadas = cmd.ExecuteNonQuery();
-                    return linhasAfetadas > 0;
-                }
-            }
-
-            public bool ExcluirTransporte(int id)
-            {
-                using (var conexao = new MySqlConnection(_conexaoMySQL))
-                {
-                    conexao.Open();
-                    var cmd = new MySqlCommand("delete from tbTransporte WHERE IdTransporte=@id", conexao);
-                    cmd.Parameters.AddWithValue("@id", id);
-                    int linhasAfetadas = cmd.ExecuteNonQuery();
-                    return linhasAfetadas > 0;
-                }
-            }
-
-            public tbTransporte ObterTransporte(int id)
-            {
-                using (var conexao = new MySqlConnection(_conexaoMySQL))
-                {
-                    conexao.Open();
-                    var cmd = new MySqlCommand("select * from tbTransporte WHERE IdTransporte=@id", conexao);
-                    cmd.Parameters.AddWithValue("@id", id);
-
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            return new tbTransporte
-                            {
-                                IdTransporte = Convert.ToInt32(reader["IdTransporte"]),
-                                CodigoTransporte = reader["CodigoTransporte"].ToString(),
-                                Companhia = reader["Companhia"].ToString(),
-                                TipoTransporte = reader["TipoTransporte"].ToString()
-                            };
-                        }
-                    }
-                }
-                return null;
-            }
-
-            public List<tbTransporte> ListarTransportes()
-            {
-                var lista = new List<tbTransporte>();
-                using (var conexao = new MySqlConnection(_conexaoMySQL))
-                {
-                conexao.Open();
-                    var cmd = new MySqlCommand("select * from tbTransporte", conexao);
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            lista.Add(new tbTransporte
-                            {
-                                IdTransporte = Convert.ToInt32(reader["IdTransporte"]),
-                                CodigoTransporte = reader["CodigoTransporte"].ToString(),
-                                Companhia = reader["Companhia"].ToString(),
-                                TipoTransporte = reader["TipoTransporte"].ToString()
-                            });
-                        }
-                    }
-                }
-                return lista;
-            }
+        public TransporteRepositorio(IConfiguration configuration)
+        {
+            _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
         }
 
+        public async Task<bool> CadastrarTransporte(tbTransporte transporte)
+        {
+            await using var conexao = new MySqlConnection(_conexaoMySQL);
+            await conexao.OpenAsync();
+
+            var cmd = new MySqlCommand("insert into tbTransporte (CodigoTransporte, Companhia, TipoTransporte) VALUES (@codigo, @companhia, @tipo)", conexao);
+            cmd.Parameters.AddWithValue("@codigo", transporte.CodigoTransporte);
+            cmd.Parameters.AddWithValue("@companhia", transporte.Companhia);
+            cmd.Parameters.AddWithValue("@tipo", transporte.TipoTransporte);
+
+            int linhasAfetadas = await cmd.ExecuteNonQueryAsync();
+            return linhasAfetadas > 0;
+        }
+
+        public async Task<bool> AtualizarTransporte(tbTransporte transporte)
+        {
+            await using var conexao = new MySqlConnection(_conexaoMySQL);
+            await conexao.OpenAsync();
+
+            var cmd = new MySqlCommand("update tbTransporte set CodigoTransporte=@codigo, Companhia=@companhia, TipoTransporte=@tipo WHERE IdTransporte=@id", conexao);
+            cmd.Parameters.AddWithValue("@codigo", transporte.CodigoTransporte);
+            cmd.Parameters.AddWithValue("@companhia", transporte.Companhia);
+            cmd.Parameters.AddWithValue("@tipo", transporte.TipoTransporte);
+            cmd.Parameters.AddWithValue("@id", transporte.IdTransporte);
+
+            int linhasAfetadas = await cmd.ExecuteNonQueryAsync();
+            return linhasAfetadas > 0;
+        }
+
+        public async Task<bool> ExcluirTransporte(int id)
+        {
+            await using var conexao = new MySqlConnection(_conexaoMySQL);
+            await conexao.OpenAsync();
+
+            var cmd = new MySqlCommand("delete from tbTransporte WHERE IdTransporte=@id", conexao);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            int linhasAfetadas = await cmd.ExecuteNonQueryAsync();
+            return linhasAfetadas > 0;
+        }
+
+        public async Task<tbTransporte> ObterTransporte(int id)
+        {
+            await using var conexao = new MySqlConnection(_conexaoMySQL);
+            await conexao.OpenAsync();
+
+            var cmd = new MySqlCommand("select * from tbTransporte WHERE IdTransporte=@id", conexao);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            await using var reader = await cmd.ExecuteReaderAsync();
+            if (await reader.ReadAsync())
+            {
+                return new tbTransporte
+                {
+                    IdTransporte = reader.GetInt32("IdTransporte"),
+                    CodigoTransporte = reader.GetString("CodigoTransporte"),
+                    Companhia = reader.GetString("Companhia"),
+                    TipoTransporte = reader.GetString("TipoTransporte")
+                };
+            }
+
+            return null;
+        }
+
+        public async Task<List<tbTransporte>> ListarTransportes()
+        {
+            var lista = new List<tbTransporte>();
+
+            await using var conexao = new MySqlConnection(_conexaoMySQL);
+            await conexao.OpenAsync();
+
+            var cmd = new MySqlCommand("select * from tbTransporte", conexao);
+
+            await using var reader = await cmd.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                lista.Add(new tbTransporte
+                {
+                    IdTransporte = reader.GetInt32("IdTransporte"),
+                    CodigoTransporte = reader.GetString("CodigoTransporte"),
+                    Companhia = reader.GetString("Companhia"),
+                    TipoTransporte = reader.GetString("TipoTransporte")
+                });
+            }
+
+            return lista;
+        }
     }
+}
