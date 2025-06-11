efcf0f32ba7593bd057476ba9486f0571fe7e0dc
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-11 10:57:31 -0300

    alterado os métodos do repositorio Pacote para async

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/PacoteRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/PacoteRepositorio.cs
@@ -1,24 +1,30 @@
 ﻿using MySql.Data.MySqlClient;
-using Mysqlx.Crud;
 using ProjetoEcommerce.Models;
 using System.Data;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
 
 namespace ProjetoEcommerce.Repositorios
 {
-    public class PacoteRepositorio(IConfiguration configuration)
+    public class PacoteRepositorio
     {
-        private readonly string _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
+        private readonly string _conexaoMySQL;
 
-        public bool CadastrarPacote(tbPacote pacote)
+        public PacoteRepositorio(IConfiguration configuration)
+        {
+            _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
+        }
+
+        public async Task<bool> CadastrarPacote(tbPacote pacote)
         {
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
-                conexao.Open();
+                await conexao.OpenAsync();
 
                 MySqlCommand cmdBuscarPassagem = new MySqlCommand("select 1 from tbPassagem where IdPassagem=@idPassagem", conexao);
                 cmdBuscarPassagem.Parameters.AddWithValue("@idPassagem", pacote.IdPassagem);
 
-                using (var drBuscarPassagem = cmdBuscarPassagem.ExecuteReader())
+                using (var drBuscarPassagem = await cmdBuscarPassagem.ExecuteReaderAsync())
                 {
                     if (!drBuscarPassagem.HasRows)
                     {
@@ -26,10 +32,11 @@ namespace ProjetoEcommerce.Repositorios
                         return false;
                     }
                 }
+
                 MySqlCommand cmdBuscarProduto = new MySqlCommand("select 1 from tbProduto where IdProduto=@idProduto", conexao);
                 cmdBuscarProduto.Parameters.AddWithValue("@idProduto", pacote.IdProduto);
 
-                using (var drBuscarProduto = cmdBuscarProduto.ExecuteReader())
+                using (var drBuscarProduto = await cmdBuscarProduto.ExecuteReaderAsync())
                 {
                     if (!drBuscarProduto.HasRows)
                     {
@@ -44,67 +51,77 @@ namespace ProjetoEcommerce.Repositorios
                 cmdInsertPacote.Parameters.AddWithValue("@nomePacote", pacote.NomePacote);
                 cmdInsertPacote.Parameters.AddWithValue("@descricao", pacote.Descricao);
                 cmdInsertPacote.Parameters.AddWithValue("@valor", pacote.Valor);
-                cmdInsertPacote.ExecuteNonQuery();
+
+                await cmdInsertPacote.ExecuteNonQueryAsync();
+
                 return true;
             }
         }
 
-        public bool AtualizarPacote(tbPacote pacote)
+        public async Task<bool> AtualizarPacote(tbPacote pacote)
         {
-            using(var conexao = new MySqlConnection(_conexaoMySQL))
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
-                MySqlCommand cmdAtualizarPacote = new MySqlCommand("update tbPacote set NomePacote=@nomePacote, Descricao=@descricao, Valor=@valor, IdPassagem=idProduto, IdProduto=@idProduto" + "where IdPacote=@idPacote");
+                await conexao.OpenAsync();
+
+                MySqlCommand cmdAtualizarPacote = new MySqlCommand("update tbPacote set NomePacote=@nomePacote, Descricao=@descricao, Valor=@valor, IdPassagem=@idPassagem, IdProduto=@idProduto where IdPacote=@idPacote", conexao);
                 cmdAtualizarPacote.Parameters.AddWithValue("@nomePacote", pacote.NomePacote);
                 cmdAtualizarPacote.Parameters.AddWithValue("@descricao", pacote.Descricao);
                 cmdAtualizarPacote.Parameters.AddWithValue("@valor", pacote.Valor);
                 cmdAtualizarPacote.Parameters.AddWithValue("@idPassagem", pacote.IdPassagem);
                 cmdAtualizarPacote.Parameters.AddWithValue("@idProduto", pacote.IdProduto);
-                int linhasAfetadas = cmdAtualizarPacote.ExecuteNonQuery();
+                cmdAtualizarPacote.Parameters.AddWithValue("@idPacote", pacote.IdPacote);
+
+                int linhasAfetadas = await cmdAtualizarPacote.ExecuteNonQueryAsync();
+
                 return linhasAfetadas > 0;
             }
         }
-        public void ExcluirPacote(int Id)
-        {
-            using(var conexao = new MySqlConnection(_conexaoMySQL))
-            {
-                conexao.Open();
-                MySqlCommand cmd = new MySqlCommand("delete from tbPacote where IdPacote=@idPacote", conexao);
-                cmd.Parameters.AddWithValue("@IdPacote", Id);
-                int i = cmd.ExecuteNonQuery();
-                conexao.Close() ;
-            }
-        }
-        public tbPacote ObterPacote(int Codigo)
+
+        public async Task ExcluirPacote(int Id)
         {
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
-                conexao.Open();
+                await conexao.OpenAsync();
+
+                MySqlCommand cmd = new MySqlCommand("delete from tbPacote where IdPacote=@idPacote", conexao);
+                cmd.Parameters.AddWithValue("@idPacote", Id);
+
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+
+        public async Task<tbPacote> ObterPacote(int Codigo)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                await conexao.OpenAsync();
+
                 MySqlCommand cmd = new MySqlCommand("select * from tbPacote where IdPacote=@codigo", conexao);
                 cmd.Parameters.AddWithValue("@codigo", Codigo);
 
-                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                MySqlDataReader dr;
                 tbPacote pacote = new tbPacote();
 
-                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
-
-                while (dr.Read())
+                using (var dr = await cmd.ExecuteReaderAsync(CommandBehavior.CloseConnection))
                 {
-                    pacote.IdPacote = Convert.ToInt32(dr["IdPacote"]);
-                    pacote.NomePacote = ((string) dr["NomePacote"]);
-                    pacote.Descricao = ((string) dr["Descricao"]);
-                    pacote.Valor = (decimal) (dr["Preco"]);
-                    pacote.IdPassagem = new tbPassagem
+                    while (await dr.ReadAsync())
                     {
-                         IdPassagem = Convert.ToInt32(dr["IdPassagem"])
-                    };
-                    pacote.IdProduto = new tbProduto
-                    {
-                        IdProduto = Convert.ToInt32(dr["IdProduto"])
-                    };
+                        pacote.IdPacote = Convert.ToInt32(dr["IdPacote"]);
+                        pacote.NomePacote = (string)dr["NomePacote"];
+                        pacote.Descricao = (string)dr["Descricao"];
+                        pacote.Valor = (decimal)dr["Valor"];
+                        pacote.IdPassagem = new tbPassagem
+                        {
+                            IdPassagem = Convert.ToInt32(dr["IdPassagem"])
+                        };
+                        pacote.IdProduto = new tbProduto
+                        {
+                            IdProduto = Convert.ToInt32(dr["IdProduto"])
+                        };
+                    }
                 }
                 return pacote;
             }
-      }
+        }
     }
 }
