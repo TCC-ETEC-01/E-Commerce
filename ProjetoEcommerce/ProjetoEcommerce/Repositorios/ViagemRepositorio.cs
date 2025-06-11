b7cb8be4e9fe599b06ac596c440b93764b7447ec
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-11 11:19:16 -0300

    alterado os métodos do repositorio Viagem para async

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/ViagemRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/ViagemRepositorio.cs
@@ -1,50 +1,57 @@
 ﻿using ProjetoEcommerce.Models;
 using MySql.Data.MySqlClient;
 using System.Data;
-using System.Security.Cryptography;
-using Microsoft.VisualBasic;
+using System.Threading.Tasks;
+
 namespace ProjetoEcommerce.Repositorios
 {
-    public class ViagemRepositorio (IConfiguration configuration)
+    public class ViagemRepositorio
     {
-        private readonly string _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
+        private readonly string _conexaoMySQL;
 
-        public void CadastrarViagem(tbViagem viagem)
+        public ViagemRepositorio(IConfiguration configuration)
         {
-            using (var conexao = new MySqlConnection(_conexaoMySQL))
-            {
-                conexao.Open();
-                MySqlCommand cmd = new MySqlCommand("insert into tbViagem(DataRetorno,Descricao,Origem,Destino,DataPartida)values" +
-                    "(@dataretorno,@descricao,@origem,@destino,@tipotransporte,@datapartida)", conexao);
-                cmd.Parameters.Add("@dataretorno",MySqlDbType.DateTime).Value = viagem.DataRetorno;
-                cmd.Parameters.Add("@descricao", MySqlDbType.Text).Value = viagem.Descricao;
-                cmd.Parameters.Add("@origem", MySqlDbType.VarChar).Value = viagem.Origem;
-                cmd.Parameters.Add("@destino", MySqlDbType.VarChar).Value = viagem.Destino;
-                cmd.Parameters.Add("@datapartida",MySqlDbType.DateTime).Value = viagem.DataPartida;
-                cmd.ExecuteNonQuery();
-            }
+            _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
         }
-        public bool EditarViagem(tbViagem viagem)
+
+        public async Task CadastrarViagem(tbViagem viagem)
+        {
+            using var conexao = new MySqlConnection(_conexaoMySQL);
+            await conexao.OpenAsync();
+
+            MySqlCommand cmd = new MySqlCommand(
+                "insert into tbViagem(DataRetorno,Descricao,Origem,Destino,DataPartida) values" +
+                "(@dataretorno,@descricao,@origem,@destino,@datapartida)", conexao);
+
+            cmd.Parameters.Add("@dataretorno", MySqlDbType.DateTime).Value = viagem.DataRetorno;
+            cmd.Parameters.Add("@descricao", MySqlDbType.Text).Value = viagem.Descricao;
+            cmd.Parameters.Add("@origem", MySqlDbType.VarChar).Value = viagem.Origem;
+            cmd.Parameters.Add("@destino", MySqlDbType.VarChar).Value = viagem.Destino;
+            cmd.Parameters.Add("@datapartida", MySqlDbType.DateTime).Value = viagem.DataPartida;
+
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        public async Task<bool> EditarViagem(tbViagem viagem)
         {
             try
             {
+                using var conexao = new MySqlConnection(_conexaoMySQL);
+                await conexao.OpenAsync();
 
+                MySqlCommand cmd = new MySqlCommand(
+                    "update tbViagem set DataRetorno=@dataretorno,Descricao=@descricao,Origem=@origem,Destino=@destino," +
+                    "DataPartida=@datapartida where IdViagem=@IdViagem", conexao);
 
-                using (var conexao = new MySqlConnection(_conexaoMySQL))
-                {
-                    conexao.Open();
-                    MySqlCommand cmd = new MySqlCommand("update tbViagem set DataRetorno=@dataretorno,Descricao=@descricao,Origem=@origem,Destino=@destino," +
-                        "DataPartida=@datapartida" + "where IdViagem=@IdViagem", conexao);
-                    cmd.Parameters.Add("@IdViagem", MySqlDbType.Int32).Value = viagem.IdViagem;
-                    cmd.Parameters.Add("@DataRetorno", MySqlDbType.DateTime).Value = viagem.DataRetorno;
-                    cmd.Parameters.Add("@Descricao", MySqlDbType.VarChar).Value = viagem.Descricao;
-                    cmd.Parameters.Add("@Origem", MySqlDbType.VarChar).Value = viagem.Origem;
-                    cmd.Parameters.Add("@Destino", MySqlDbType.VarChar).Value = viagem.Destino;
-                    cmd.Parameters.Add("@DataPartida", MySqlDbType.VarChar).Value = viagem.DataPartida;
-                    int linhasAfetadas = cmd.ExecuteNonQuery();
+                cmd.Parameters.Add("@IdViagem", MySqlDbType.Int32).Value = viagem.IdViagem;
+                cmd.Parameters.Add("@dataretorno", MySqlDbType.DateTime).Value = viagem.DataRetorno;
+                cmd.Parameters.Add("@descricao", MySqlDbType.VarChar).Value = viagem.Descricao;
+                cmd.Parameters.Add("@origem", MySqlDbType.VarChar).Value = viagem.Origem;
+                cmd.Parameters.Add("@destino", MySqlDbType.VarChar).Value = viagem.Destino;
+                cmd.Parameters.Add("@datapartida", MySqlDbType.DateTime).Value = viagem.DataPartida;
 
-                    return linhasAfetadas > 0;
-                }
+                int linhasAfetadas = await cmd.ExecuteNonQueryAsync();
+                return linhasAfetadas > 0;
             }
             catch (MySqlException ex)
             {
@@ -52,70 +59,76 @@ namespace ProjetoEcommerce.Repositorios
                 return false;
             }
         }
-        public IEnumerable<tbViagem> TodasViagens()
+
+        public async Task<IEnumerable<tbViagem>> TodasViagens()
         {
-            List<tbViagem> ListaViagens = new List<tbViagem>();
-            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            var ListaViagens = new List<tbViagem>();
+
+            using var conexao = new MySqlConnection(_conexaoMySQL);
+            await conexao.OpenAsync();
+
+            MySqlCommand cmd = new MySqlCommand("select * from tbViagem", conexao);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            // O Fill não tem versão async, então mantém sync
+            da.Fill(dt);
+
+            conexao.Close();
+
+            foreach (DataRow dr in dt.Rows)
             {
-                conexao.Open();
-                MySqlCommand cmd = new MySqlCommand("select * from tbViagem", conexao);
-                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-
-                da.Fill(dt);
-                conexao.Close();
-
-                foreach (DataRow dr in dt.Rows)
+                ListaViagens.Add(new tbViagem
                 {
-                    ListaViagens.Add(new tbViagem
-                    {
-                        IdViagem = Convert.ToInt32(dr["IdViagem"]),
-                        DataRetorno = ((DateTime)dr["DataRetorno"]),
-                        Descricao = ((string)dr["Descricao"]),
-                        Origem = ((string)dr["Origem"]),
-                        Destino = ((string)dr["Destino"]),
-                        DataPartida = ((DateTime)dr["DataPartida"])
-                    });
-                } 
-                return ListaViagens;
+                    IdViagem = Convert.ToInt32(dr["IdViagem"]),
+                    DataRetorno = (DateTime)dr["DataRetorno"],
+                    Descricao = (string)dr["Descricao"],
+                    Origem = (string)dr["Origem"],
+                    Destino = (string)dr["Destino"],
+                    DataPartida = (DateTime)dr["DataPartida"]
+                });
             }
-        }
-        public void ExcluirViagem(int Id)
-        {
-            using (var conexao = new MySqlConnection(_conexaoMySQL))
-            {
-                conexao.Open();
-                MySqlCommand cmd = new MySqlCommand("delete from tbViagem where IdViagem=@Id", conexao);
-                cmd.Parameters.AddWithValue("IdViagem", Id);
-                int i = cmd.ExecuteNonQuery();
-                conexao.Close();
-            }
-        }
-        public tbViagem ObterViagem(int Id)
-        {
-            using (var conexao = new MySqlConnection(_conexaoMySQL))
-            {
-                conexao.Open();
-                MySqlCommand cmd = new MySqlCommand("select * from tbViagem where IdViagem=@IdViagem", conexao);
-                cmd.Parameters.Add("IdViagem",MySqlDbType.VarChar).Value = Id;
 
-                using(MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+            return ListaViagens;
+        }
+
+        public async Task ExcluirViagem(int Id)
+        {
+            using var conexao = new MySqlConnection(_conexaoMySQL);
+            await conexao.OpenAsync();
+
+            MySqlCommand cmd = new MySqlCommand("delete from tbViagem where IdViagem=@IdViagem", conexao);
+            cmd.Parameters.AddWithValue("@IdViagem", Id);
+            await cmd.ExecuteNonQueryAsync();
+
+            conexao.Close();
+        }
+
+        public async Task<tbViagem> ObterViagem(int Id)
+        {
+            using var conexao = new MySqlConnection(_conexaoMySQL);
+            await conexao.OpenAsync();
+
+            MySqlCommand cmd = new MySqlCommand("select * from tbViagem where IdViagem=@IdViagem", conexao);
+            cmd.Parameters.Add("@IdViagem", MySqlDbType.Int32).Value = Id;
+
+            using var dr = (MySqlDataReader)await cmd.ExecuteReaderAsync(CommandBehavior.CloseConnection);
+
+            tbViagem viagem = null;
+            if (await dr.ReadAsync())
+            {
+                viagem = new tbViagem
                 {
-                    tbViagem viagem = null;
-                    if(dr.Read())
-                    {
-                        viagem = new tbViagem
-                        {
-                            IdViagem = Convert.ToInt32(dr["IdViagem"]),
-                            DataRetorno = ((DateTime)dr["DataRetorno"]),
-                            Descricao = ((string)dr["Descricao"]),
-                            Origem = ((string)dr["Origem"]),
-                            Destino = ((string)dr["Destino"]),
-                            DataPartida = ((DateTime)dr["DataPartida"])
-                        };
-                    } return viagem;
-                }
+                    IdViagem = Convert.ToInt32(dr["IdViagem"]),
+                    DataRetorno = (DateTime)dr["DataRetorno"],
+                    Descricao = (string)dr["Descricao"],
+                    Origem = (string)dr["Origem"],
+                    Destino = (string)dr["Destino"],
+                    DataPartida = (DateTime)dr["DataPartida"]
+                };
             }
+
+            return viagem;
         }
     }
 }
