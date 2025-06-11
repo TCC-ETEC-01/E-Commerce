9b159b4dfc5adbd600643c695d6dbabe872ebae3
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-11 10:43:42 -0300

    alterado os métodos do clienteRepositorio para async

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/ClienteRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/ClienteRepositorio.cs
@@ -9,18 +9,18 @@ namespace ProjetoEcommerce.Repositorios
     {
         private readonly string _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
 
-        public tbCliente ObterCliente(int Id)
+        public async Task<tbCliente> ObterCliente(int Id)
         {
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
-                conexao.Open();
-                MySqlCommand cmd = new MySqlCommand ("select * from tbCliente where IdCliente = @idCliente", conexao);
+                await conexao.OpenAsync();
+                MySqlCommand cmd = new MySqlCommand("select * from tbCliente where IdCliente = @idCliente", conexao);
                 cmd.Parameters.Add("@idCliente", MySqlDbType.VarChar).Value = Id;
 
-                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                using (MySqlDataReader dr = (MySqlDataReader)await cmd.ExecuteReaderAsync(CommandBehavior.CloseConnection))
                 {
                     tbCliente cliente = null;
-                    if (dr.Read())
+                    if (await dr.ReadAsync())
                     {
                         cliente = new tbCliente
                         {
@@ -36,15 +36,16 @@ namespace ProjetoEcommerce.Repositorios
                 }
             }
         }
+
         public async Task<bool> CadastrarCliente(tbCliente cliente)
         {
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
-               await conexao.OpenAsync();
+                await conexao.OpenAsync();
 
                 MySqlCommand verify = new MySqlCommand("select 1 from tbCliente where Email=@email", conexao);
                 verify.Parameters.AddWithValue("@email", cliente.Email);
-                using (var conf =  await verify.ExecuteReaderAsync())
+                using (var conf = await verify.ExecuteReaderAsync())
                 {
                     if (conf.HasRows)
                     {
@@ -52,10 +53,10 @@ namespace ProjetoEcommerce.Repositorios
                         return false;
                     }
                 }
+
                 MySqlCommand verifyCpf = new MySqlCommand("select 1 from tbCliente where Cpf=@cpf", conexao);
-              
                 verifyCpf.Parameters.AddWithValue("@cpf", cliente.Cpf);
-                using (var conf = await verify.ExecuteReaderAsync())
+                using (var conf = await verifyCpf.ExecuteReaderAsync())
                 {
                     if (conf.HasRows)
                     {
@@ -63,11 +64,12 @@ namespace ProjetoEcommerce.Repositorios
                         return false;
                     }
                 }
+
                 MySqlCommand verifyTelefone = new MySqlCommand("select 1 from tbCliente where Telefone=@telefone", conexao);
                 verifyTelefone.Parameters.AddWithValue("@telefone", cliente.Telefone);
-                using (var conf = await verify.ExecuteReaderAsync())
+                using (var conf = await verifyTelefone.ExecuteReaderAsync())
                 {
-                    if (conf.HasRows )
+                    if (conf.HasRows)
                     {
                         Console.WriteLine("Telefone ja cadastrado!");
                         return false;
@@ -80,28 +82,25 @@ namespace ProjetoEcommerce.Repositorios
                 cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = cliente.Email;
                 cmd.Parameters.Add("@telefone", MySqlDbType.VarChar).Value = cliente.Telefone;
                 cmd.Parameters.Add("@cpf", MySqlDbType.VarChar).Value = cliente.Cpf;
-                cmd.ExecuteNonQuery();
+                await cmd.ExecuteNonQueryAsync();
                 return true;
-
             }
         }
 
-        public bool EditarCliente(tbCliente cliente)
+        public async Task<bool> EditarCliente(tbCliente cliente)
         {
             try
             {
-
                 using (var conexao = new MySqlConnection(_conexaoMySQL))
                 {
-                    conexao.Open();
-                    MySqlCommand cmd = new MySqlCommand("update tbCliente set Nome=@nome,Sexo=@sexo,Email=@email,Telefone=@telefone,Cpf=@cpf" +
-                        "where IdCliente=@IdCliente", conexao);
+                    await conexao.OpenAsync();
+                    MySqlCommand cmd = new MySqlCommand("update tbCliente set Nome=@nome,Sexo=@sexo,Email=@email,Telefone=@telefone,Cpf=@cpf where IdCliente=@IdCliente", conexao);
                     cmd.Parameters.Add("@IdCliente", MySqlDbType.Int32).Value = cliente.IdCliente;
                     cmd.Parameters.Add("@Nome", MySqlDbType.VarChar).Value = cliente.Nome;
                     cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = cliente.Email;
                     cmd.Parameters.Add("@Telefone", MySqlDbType.VarChar).Value = cliente.Telefone;
                     cmd.Parameters.Add("@Cpf", MySqlDbType.VarChar).Value = cliente.Cpf;
-                    int linhasAfestadas = cmd.ExecuteNonQuery();
+                    int linhasAfestadas = await cmd.ExecuteNonQueryAsync();
                     return linhasAfestadas > 0;
                 }
             }
@@ -111,6 +110,7 @@ namespace ProjetoEcommerce.Repositorios
                 return false;
             }
         }
+
         public async Task<IEnumerable<tbCliente>> TodosClientes()
         {
             List<tbCliente> ListaCliente = new List<tbCliente>();
@@ -122,7 +122,7 @@ namespace ProjetoEcommerce.Repositorios
                 DataTable dt = new DataTable();
                 da.Fill(dt);
                 conexao.Close();
-                foreach ( DataRow dr in dt.Rows)
+                foreach (DataRow dr in dt.Rows)
                 {
                     ListaCliente.Add(new tbCliente
                     {
@@ -136,17 +136,17 @@ namespace ProjetoEcommerce.Repositorios
                 return ListaCliente;
             }
         }
-        public void ExcluirCliente( int id)
+
+        public async Task ExcluirCliente(int id)
         {
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
-                conexao.Open();
-                MySqlCommand cmd = new MySqlCommand("delete from tbCliente where IdCliente=@IdCliente");
+                await conexao.OpenAsync();
+                MySqlCommand cmd = new MySqlCommand("delete from tbCliente where IdCliente=@IdCliente", conexao);
                 cmd.Parameters.AddWithValue("IdCliente", id);
-                int i = cmd.ExecuteNonQuery();
-                conexao.Close();
+                await cmd.ExecuteNonQueryAsync();
+                await conexao.CloseAsync();
             }
         }
     }
 }
-
