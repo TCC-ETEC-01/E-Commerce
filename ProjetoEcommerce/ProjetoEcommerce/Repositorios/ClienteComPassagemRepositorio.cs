015380f8776d76866834012af8c6c38fde8dd029
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-06-12 15:09:12 -0300

    alterando a query do inner join

--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/ClienteComPassagemRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/ClienteComPassagemRepositorio.cs
@@ -1,6 +1,50 @@
-﻿namespace ProjetoEcommerce.Repositorios
+﻿using MySql.Data.MySqlClient;
+using ProjetoEcommerce.Models;
+using System.Configuration;
+
+namespace ProjetoEcommerce.Repositorios
 {
-    public class ClienteComPassagemRepositorio
+    public class ClienteComPassagemRepositorio(IConfiguration configuration)
     {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
+
+        public async Task<IEnumerable<tbPassagemComViagem>> PassagemComViagem()
+        {
+            var lista = new List<tbPassagemComViagem>();
+
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                await conexao.OpenAsync();
+
+                string query = @"select c.IdCliente, c.Nome as Nome, c.Email,
+	                             p.IdPassagem, p.Assento, p.Valor, p.Situacao
+	                             from tbPassagem p
+	                             inner join tbCliente c on p.IdCliente = c.IdCliente;";
+
+                using (MySqlCommand join = new MySqlCommand(query, conexao))
+                {
+                    using (var drPassagemComViagem = await join.ExecuteReaderAsync())
+                    {
+                        while (await drPassagemComViagem.ReadAsync())
+                        {
+                            lista.Add(new tbPassagemComViagem
+                            {
+                                IdPassagem = drPassagemComViagem.GetInt32("IdPassagem"),
+                                Origem = drPassagemComViagem.GetString("Origem"),
+                                Destino = drPassagemComViagem.GetString("Destino"),
+                                Descricao = drPassagemComViagem.GetString("Descricao"),
+                                DataPartida = drPassagemComViagem.GetDateTime("Partida"),
+                                DataRetorno = drPassagemComViagem.GetDateTime("Retorno"),
+                                TipoTransporte = drPassagemComViagem.GetString("Transporte"),
+                                CodigoTransporte = drPassagemComViagem.GetString("Cod_Transporte"),
+                                Companhia = drPassagemComViagem.GetString("Companhia"),
+                                Valor = drPassagemComViagem.GetDecimal("Valor")
+                            });
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
     }
 }
--- ProjetoEcommerce/ProjetoEcommerce/Repositorios/PassagemComViagemRepositorio.cs
+++ ProjetoEcommerce/ProjetoEcommerce/Repositorios/PassagemComViagemRepositorio.cs
@@ -16,13 +16,10 @@ namespace ProjetoEcommerce.Repositorios
             {
                 await conexao.OpenAsync();
 
-                string query = @"select  p.IdPassagem, p.Valor, v.Origem, v.Destino, p.Assento, v.Descricao, 
-                        v.DataPartida as Partida, v.DataRetorno as Retorno, 
-                        t.TipoTransporte as Transporte, t.Companhia, 
-                        t.CodigoTransporte as Cod_Transporte
-                        from tbPassagem p
-                        inner join tbViagem v on p.IdViagem = v.IdViagem
-                        inner join tbTransporte t on p.IdTransporte = t.IdTransporte;";
+                string query = @"select c.IdCliente, c.Nome as Nome, c.Email, c.Cpf, c.Telefone,
+	                            p.IdPassagem, p.Assento, p.Valor, p.Situacao, p.DataCompra, p.IdViagem, P.Translado
+	                            from tbPassagem p
+	                            inner join tbCliente c on p.IdCliente = c.IdCliente;";
 
                 using (MySqlCommand join = new MySqlCommand(query, conexao))
                 {
