728b7440b1dcbbd901d84d07089880b12a1cc429
Amanda <amandapcardoso981@gamil.com>
2025-06-13 18:03:08 -0300

    Criando barra de pesquisa ClienteIndex

---
+++ ProjetoEcommerce/ProjetoEcommerce/wwwroot/js/ClienteIndex.js
@@ -0,0 +1,15 @@
+﻿<script>
+    function searchTable() {
+        var input = document.getElementById("searchInput").value.toLowerCase();
+    var rows = document.querySelectorAll("table tbody tr");
+
+    rows.forEach(function (row) {
+            var text = row.textContent.toLowerCase();
+    if (text.includes(input)) {
+        row.style.display = "";
+            } else {
+        row.style.display = "none";
+            }
+        });
+    }
+</script>
